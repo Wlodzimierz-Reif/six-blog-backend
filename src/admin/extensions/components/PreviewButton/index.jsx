396d3a98474f738452972c8a93101be708cd2ff4
Wlodek Reif <wlodek.reif@six.agency>
2023-02-14 16:24:22 +0000

    styled the preview button

--- src/admin/extensions/components/PreviewButton/index.jsx
+++ src/admin/extensions/components/PreviewButton/index.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { Button } from "@strapi/design-system/Button";
+
 // import Up from "@strapi/icons/Up";
 
 const PreviewButton = () => {
- const postIdArray = window.location.href.split("/")
- console.log(postIdArray)
- const postId = postIdArray[postIdArray.length - 1]
- console.log(process.env.PREVIEW_SECRET)
+  const postIdArray = window.location.href.split("/");
+  console.log(postIdArray);
+  const postId = postIdArray[postIdArray.length - 1];
+  console.log(process.env.PREVIEW_SECRET);
   return (
-    <a href={`http://localhost:3000/api/preview?secret=${process.env.PREVIEW_SECRET}&slug=post-2`} target="_blank">
+    <a
+      href={`http://localhost:3000/api/preview?secret=${process.env.PREVIEW_SECRET}&slug=post-2`}
+      target="_blank"
+      style={{ textDecoration: "none" }}
+    >
       <Button
-        variant="secondary"
+        fullWidth
+        variant="default"
         //   startIcon={<Up />}
         // onClick={() => alert(window.location.href)}
       >
-        Hello World
+        Preview post
       </Button>
+      {/* <div>test</div> */}
     </a>
   );
 };
